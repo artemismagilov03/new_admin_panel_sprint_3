0ae8467b807bcdbe5f1f07bcfaf1b383b2822072
ArtemIsmagilov <118372045+ArtemIsmagilov@users.noreply.github.com>
2024-06-28 14:14:37 +0400

    filter by genre.updated_at

--- etl/sqls/fetch_genres.sql
+++ etl/sqls/fetch_genres.sql
@@ -2,5 +2,5 @@ SELECT to_json(sub_query)
 FROM (SELECT id   AS id,
              name AS name
       FROM genre
-      -- WHERE fw.updated_at > NOW()
+      -- WHERE updated_at > NOW()
       ORDER BY id) AS sub_query;
