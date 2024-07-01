906c9a5eba92221922909833a73554bba0e2156a
artemismagilov03 <artem.ismagilov.03@gmail.com>
2024-07-01 13:35:00 +0400

    backoff working based on indexing schemas with create connection for every operation migrate

--- etl/etl_pipelines.py
+++ etl/etl_pipelines.py
@@ -1,11 +1,12 @@
 from contextlib import contextmanager
 from time import sleep
 
-import config
 import psycopg
 from elasticsearch import Elasticsearch
-from etl_logger import logger
-from etl_utils import backoff
+
+from etl import config
+from etl.etl_logger import logger
+from etl.etl_utils import backoff
 
 
 class ETL:
@@ -25,54 +26,54 @@ class ETL:
         with Elasticsearch(f'http://{config.ELASTIC_HOST}:{config.ELASTIC_PORT}/') as es:
             yield es
 
-    def pipeline_film_works(self, pg_conn: psycopg.Cursor, es_conn: Elasticsearch):
-        self.indexing_es('movies', 'sqls/fetch_film_works.sql', pg_conn, es_conn)
+    def pipeline_film_works(self):
+        self.indexing_es('movies', 'sqls/fetch_film_works.sql')
         logger.info('Successfully indexing film works')
 
-    def pipeline_genres(self, pg_conn: psycopg.Cursor, es_conn: Elasticsearch):
-        self.indexing_es('genres', 'sqls/fetch_genres.sql', pg_conn, es_conn)
+    def pipeline_genres(self):
+        self.indexing_es('genres', 'sqls/fetch_genres.sql')
         logger.info('Successfully indexing genres')
 
-    def pipeline_persons(self, pg_conn: psycopg.Cursor, es_conn: Elasticsearch):
-        self.indexing_es('persons', 'sqls/fetch_persons.sql', pg_conn, es_conn)
+    def pipeline_persons(selfh):
+        self.indexing_es('persons', 'sqls/fetch_persons.sql')
         logger.info('Successfully indexing persons')
 
-    def indexing_es(self, index: str, sql_path: str, pg_conn: psycopg.Cursor, es_conn: Elasticsearch):
-        with open(sql_path) as f:
-            sql = f.read()
+    @backoff(steps=3)
+    def indexing_es(self, index: str, sql_path: str):
+        with self.create_conn_pg() as pg_conn, self.create_conn_es() as es_conn:
+            with open(sql_path) as f:
+                sql = f.read()
 
-        pg_conn.execute(sql, (self.offset,))
-        data = pg_conn.fetchmany(config.SIZE_CHUNK)
-
-        while data:
-            body = []
-            for row in data:
-                body.extend(
-                    (
-                        {
-                            'index': {
-                                '_index': index,
-                                '_id': row[0]['id'],
-                            }
-                        },
-                        row[0],
-                    )
-                )
-
-            result = es_conn.bulk(body=body)
-            assert result['errors'] is False, 'ElasticSearch bulk operation failed.'
+            pg_conn.execute(sql, (self.offset,))
             data = pg_conn.fetchmany(config.SIZE_CHUNK)
 
-            self.offset += config.SIZE_CHUNK
+            while data:
+                body = []
+                for row in data:
+                    body.extend(
+                        (
+                            {
+                                'index': {
+                                    '_index': index,
+                                    '_id': row[0]['id'],
+                                }
+                            },
+                            row[0],
+                        )
+                    )
 
-        self.offset = 0
+                result = es_conn.bulk(body=body)
+                assert result['errors'] is False, 'ElasticSearch bulk operation failed.'
+                data = pg_conn.fetchmany(config.SIZE_CHUNK)
 
-    @backoff(steps=3)
+                self.offset += config.SIZE_CHUNK
+
+            self.offset = 0
+
     def run(self):
-        with self.create_conn_pg() as pg_conn, self.create_conn_es() as es_conn:
-            self.pipeline_film_works(pg_conn, es_conn)
-            self.pipeline_genres(pg_conn, es_conn)
-            self.pipeline_persons(pg_conn, es_conn)
+        self.pipeline_film_works()
+        self.pipeline_genres()
+        self.pipeline_persons()
         logger.info('ETL process successfully finished')
 
 
