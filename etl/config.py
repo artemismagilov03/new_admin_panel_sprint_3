a1fa2f3c34f2b6a0ea9313d2317dc7c1605f304d
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2024-07-06 19:48:14 +0400

    add constants elastic index names

--- etl/config.py
+++ etl/config.py
@@ -10,3 +10,7 @@ POSTGRES_PORT = int(os.environ.get('POSTGRES_PORT', 5432))
 
 ELASTIC_HOST = os.environ.get('ELASTIC_HOST', '127.0.0.1')
 ELASTIC_PORT = int(os.getenv('ELASTIC_PORT', 9200))
+
+MOVIES_INDEX = os.environ.get('MOVIES_INDEX', 'movies')
+GENRE_INDEX = os.environ.get('GENRES_INDEX', 'genres')
+PERSONS_INDEX = os.environ.get('PERSONS_INDEX', 'persons')
